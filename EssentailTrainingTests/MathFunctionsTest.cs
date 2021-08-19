79507c41ba618c4a17389f5f57526166508a1877
Sanushi-Salgado <sanushisalgado@gmail.com>
2021-08-19 16:01:06 +0530

    Modifications

---
+++ EssentailTrainingTests/MathFunctionsTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+using System;
+
+namespace EssentailTrainingTests
+{
+    [TestClass]
+    public class MathFunctionsTest
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            MathFunctions testInstance = new MathFunctions();
+            double testResult = testInstance.Add(10, 4);
+            Assert.AreEqual(14, testResult, "Test Failed! Expected result is 14.");
+        }
+
+        [TestMethod]
+        public void Test_SubtractMethod()
+        {
+            MathFunctions testInstance = new MathFunctions();
+            double testResult = testInstance.Subtract(10, 4);
+            Assert.AreEqual(6, testResult, "Test Failed! Expected result is 6.");
+        }
+
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            MathFunctions testInstance = new MathFunctions();
+            double testResult = testInstance.Multiply(10, 4);
+            Assert.AreEqual(40, testResult, "Test Failed! Expected result is 40.");
+        }
+
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            MathFunctions testInstance = new MathFunctions();
+            try
+            {
+                double testResult = testInstance.Divide(10, 0);
+                Assert.AreEqual(2, testResult, "Test Failed!");
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ EssentialTraining/MathFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EssentialTraining
+{
+    public class MathFunctions
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+                throw new ArithmeticException("Division by zero!");
+            else
+                return num1 / num2;
+        }
+
+    }
+}
